cc1aa539911b140dc6f4387006cdaad1e4b8af21
Xavier Carriba <xavier.carriba@gmail.com>
2022-11-19 16:45:58 +0100

    Update behaviour

--- src/projects/AddProject.test.tsx
+++ src/projects/AddProject.test.tsx
@@ -1,15 +1,24 @@
 import { screen, render, fireEvent } from '@testing-library/react';
 import AddProject from './AddProject';
+import TestApp from '../../tests/TestApp';
 
 describe('AddProject', () => {
   test('Should show dialog closed by default', async () => {
-    render(<AddProject />);
+    render(
+      <TestApp>
+        <AddProject />
+      </TestApp>
+    );
 
     expect(screen.queryByTestId('add-project-dialog')).toBeNull();
   });
 
   test('Should open the dialog on add button click', async () => {
-    render(<AddProject />);
+    render(
+      <TestApp>
+        <AddProject />
+      </TestApp>
+    );
 
     fireEvent.click(screen.getByTestId('add-project-button'));
 
@@ -17,7 +26,11 @@ describe('AddProject', () => {
   });
 
   test('Should close the dialog on close button click', async () => {
-    render(<AddProject />);
+    render(
+      <TestApp>
+        <AddProject />
+      </TestApp>
+    );
     fireEvent.click(screen.getByTestId('add-project-button'));
 
     fireEvent.click(screen.getByTestId('add-project-dialog-close'));
@@ -25,5 +38,62 @@ describe('AddProject', () => {
     expect(
       screen.queryByTestId('add-project-dialog-content')
     ).not.toBeVisible();
+  });
+
+  test('Should show error if name is empty', () => {
+    render(
+      <TestApp>
+        <AddProject />
+      </TestApp>
+    );
+    fireEvent.click(screen.getByTestId('add-project-button'));
+
+    fireEvent.change(screen.getByTestId('project-key'), {
+      target: { value: 'switchboard' },
+    });
+    fireEvent.click(screen.getByTestId('add-project-dialog-save'));
+
+    return expect(
+      screen.findByText('Name should not be empty')
+    ).resolves.toBeVisible();
+  });
+
+  test('Should show error if key is empty', () => {
+    render(
+      <TestApp>
+        <AddProject />
+      </TestApp>
+    );
+    fireEvent.click(screen.getByTestId('add-project-button'));
+
+    fireEvent.change(screen.getByTestId('project-name'), {
+      target: { value: 'Switchboard' },
+    });
+    fireEvent.click(screen.getByTestId('add-project-dialog-save'));
+
+    return expect(
+      screen.findByText('Key should not be empty')
+    ).resolves.toBeVisible();
+  });
+
+  test.skip('Should disable controls on add', () => {
+    render(
+      <TestApp>
+        <AddProject />
+      </TestApp>
+    );
+    fireEvent.click(screen.getByTestId('add-project-button'));
+    fireEvent.change(screen.getByTestId('project-name'), {
+      target: { value: 'Switchboard' },
+    });
+    fireEvent.change(screen.getByTestId('project-key'), {
+      target: { value: 'switchboard' },
+    });
+
+    fireEvent.click(screen.getByTestId('add-project-dialog-save'));
+
+    expect(screen.getByTestId('project-name')).toBeDisabled();
+    expect(screen.getByTestId('project-key')).toBeDisabled();
+    expect(screen.getByTestId('project-description')).toBeDisabled();
   });
 });
--- src/projects/AddProject.tsx
+++ src/projects/AddProject.tsx
@@ -1,6 +1,7 @@
 import AddIcon from '@mui/icons-material/Add';
 import {
   Button,
+  CircularProgress,
   Dialog,
   DialogActions,
   DialogContent,
@@ -8,9 +9,16 @@ import {
   Stack,
   TextField,
 } from '@mui/material';
-import React, { useState } from 'react';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import React, { useCallback, useState } from 'react';
+import {
+  Control,
+  Controller,
+  FieldErrors,
+  SubmitHandler,
+  useForm,
+} from 'react-hook-form';
 import { isBlank } from '../utils/validation';
+import { useQueryClient } from '@tanstack/react-query';
 
 type CreateFormInput = {
   name: string;
@@ -20,19 +28,8 @@ type CreateFormInput = {
 
 export default function AddProject() {
   const [open, setOpen] = useState(false);
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-    reset,
-  } = useForm<CreateFormInput>();
-
-  const onSubmit: SubmitHandler<CreateFormInput> = (data) => {
-    console.log(data);
-  };
 
   const handleOpen = () => {
-    reset();
     setOpen(true);
   };
 
@@ -49,86 +46,161 @@ export default function AddProject() {
       >
         <AddIcon /> Add
       </Button>
-      <Dialog
-        data-testid="add-project-dialog"
-        open={open}
-        onClose={handleClose}
-        fullWidth
-      >
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <DialogTitle>Add project</DialogTitle>
-          <DialogContent data-testid="add-project-dialog-content">
-            <Stack spacing={2} sx={{ paddingTop: '0.25rem' }}>
-              <Controller
-                name="name"
-                control={control}
-                defaultValue=""
-                rules={{ validate: validateFieldNotEmpty('Name') }}
-                render={({ field }) => (
-                  <TextField
-                    label="Name"
-                    error={Boolean(errors.name)}
-                    helperText={errors?.name?.message || ' '}
-                    data-testid="project-name"
-                    {...field}
-                  />
-                )}
-              />
-              <Controller
-                name="key"
-                control={control}
-                defaultValue=""
-                rules={{ validate: validateFieldNotEmpty('Key') }}
-                render={({ field }) => (
-                  <TextField
-                    label="Key"
-                    error={Boolean(errors.key)}
-                    helperText={errors?.key?.message || ' '}
-                    data-testid="project-key"
-                    {...field}
-                  />
-                )}
-              />
-              <Controller
-                name="description"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <TextField
-                    multiline
-                    label="Description"
-                    rows={3}
-                    data-testid="project-description"
-                    {...field}
-                  />
-                )}
-              />
-            </Stack>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              variant="outlined"
-              data-testid="add-project-dialog-close"
-              onClick={handleClose}
-            >
-              Cancel
-            </Button>
-            <Button
-              type="submit"
-              variant="contained"
-              data-testid="add-project-dialog-save"
-            >
-              Add
-            </Button>
-          </DialogActions>
-        </form>
-      </Dialog>
+      <AddProjectDialog open={open} handleClose={handleClose} />
     </>
+  );
+}
+
+type AddProjectDialogProps = {
+  open: boolean;
+  handleClose: VoidFunction;
+};
+
+function AddProjectDialog({ open, handleClose }: AddProjectDialogProps) {
+  const queryClient = useQueryClient();
+  const [isUploading, setIsUploading] = useState(false);
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<CreateFormInput>();
+
+  const onClose: VoidFunction = useCallback(() => {
+    handleClose();
+    setIsUploading(false);
+    reset();
+  }, [reset, handleClose]);
+
+  const onSubmit: SubmitHandler<CreateFormInput> = useCallback(
+    (data) => {
+      setIsUploading(true);
+      queryClient.invalidateQueries({ queryKey: ['projects'] });
+      onClose();
+    },
+    [queryClient, onClose]
+  );
+
+  return (
+    <Dialog
+      data-testid="add-project-dialog"
+      open={open}
+      onClose={onClose}
+      fullWidth
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <DialogTitle>Add project</DialogTitle>
+        <DialogContent data-testid="add-project-dialog-content">
+          <AddProjectFormFields
+            disabled={isUploading}
+            control={control}
+            errors={errors}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="outlined"
+            data-testid="add-project-dialog-close"
+            onClick={onClose}
+          >
+            Cancel
+          </Button>
+          <SubmitButton isUploading={isUploading} />
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+}
+
+type AddProjectFormFieldsProps = {
+  disabled: boolean;
+  control: Control<CreateFormInput>;
+  errors: FieldErrors<CreateFormInput>;
+};
+
+function AddProjectFormFields({
+  control,
+  disabled,
+  errors,
+}: AddProjectFormFieldsProps) {
+  return (
+    <Stack spacing={2} sx={{ paddingTop: '0.25rem' }}>
+      <Controller
+        name="name"
+        control={control}
+        defaultValue=""
+        rules={{ validate: validateFieldNotEmpty('Name') }}
+        render={({ field }) => (
+          <TextField
+            disabled={disabled}
+            error={Boolean(errors.name)}
+            helperText={errors?.name?.message || ' '}
+            inputProps={{ 'data-testid': 'project-name' }}
+            label="Name"
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        name="key"
+        control={control}
+        defaultValue=""
+        rules={{ validate: validateFieldNotEmpty('Key') }}
+        render={({ field }) => (
+          <TextField
+            disabled={disabled}
+            error={Boolean(errors.key)}
+            helperText={errors?.key?.message || ' '}
+            inputProps={{ 'data-testid': 'project-key' }}
+            label="Key"
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        name="description"
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <TextField
+            disabled={disabled}
+            inputProps={{ 'data-testid': 'project-description' }}
+            label="Description"
+            multiline
+            rows={3}
+            {...field}
+          />
+        )}
+      />
+    </Stack>
+  );
+}
+
+type SubmitButtonProps = {
+  isUploading: boolean;
+};
+
+function SubmitButton({ isUploading }: SubmitButtonProps) {
+  return (
+    <Button
+      type="submit"
+      variant="contained"
+      data-testid="add-project-dialog-save"
+    >
+      {isUploading && (
+        <CircularProgress
+          color="inherit"
+          size="1rem"
+          sx={{ marginRight: '0.5rem' }}
+        />
+      )}
+      Add
+    </Button>
   );
 }
 
 function validateFieldNotEmpty(name: string) {
   return function (value: string | null) {
-    return isBlank(value) && `${name} should not be empty`;
+    return isBlank(value) ? `${name} should not be empty` : undefined;
   };
 }
